b2d4e9e57457843cc0cfd5c22123d79bcb2eeda3
vjgaur <vijayendra.gaur@gmail.com>
2022-10-14 18:31:06 +0530

    http requests endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate actix_web;
+use std::{env,io};
+use actix_web::{middleware,App,HTTPServer};
+
+mod constants;
+mod like;
+mod response;
+mod tweet;
+
+#[actix_rt::main]
+async fn main()-> io::Result<()> {
+    env:set_var("Rust_Log", "actix_web=debug",actix_server=info);
+    env_logger::init();
+
+    HTTPServer::new(||{
+
+        App::new()
+                //enable logger - always register actix-web Logger middleware last
+             .wrap(middleware::Logger::default())
+             .service(tweet::list)
+             .service(tweet::get)
+             .service(tweet::create)
+             .service(tweet::delete)
+             .service(like::list)
+             .service(like::plus_one)
+             .service(like::minus_one)
+    }).bind("0.0.0.0:9090")?.run().await
+        
 }
